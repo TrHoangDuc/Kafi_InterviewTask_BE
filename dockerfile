b992c614a534b4eebe1e0718d76d3a056c79ca4c
TrHoangDuc <hoangduc.it.work@gmail.com>
2025-01-12 22:36:35 +0700

    feat: Initial commit

---
+++ dockerfile
@@ -0,0 +1,25 @@
+# Use the official Python image as the base image
+FROM python:3.10-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Install system dependencies
+# RUN apt-get update && apt-get install -y \
+#     gcc \
+#     && rm -rf /var/lib/apt/lists/*
+
+# Copy the application requirements to the container
+COPY requirements.txt ./
+
+# Install Python dependencies
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application files to the container
+COPY . .
+
+# Expose port 5000 for the Flask application
+EXPOSE 5000
+
+# Set the entry point for the container
+CMD ["python", "importCSV.py"]
---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from sqlalchemy import create_engine, Table, MetaData, text
+import pandas as pd
+import os
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+CORS(app)
+
+db_user = 'root'
+db_password = 'hoangduc1123'
+db_host = '13.250.106.255'
+db_name = 'kafi'
+engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}')
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+def upload_csv():
+    file = request.files['file']
+
+    if not file:
+        return jsonify({'error': 'No file uploaded'}), 400
+    if not allowed_file(file.filename):
+        return jsonify({'error': 'Invalid file type. Only .csv files are allowed.'}), 400
+    filename = os.path.splitext(file.filename)[0]
+    df = pd.read_csv(file)
+    # Write CSV data to MySQL table
+    df.to_sql(filename, con=engine, if_exists='replace', index=False)
+    # Query the inserted data
+    with engine.connect() as connection:
+        result = connection.execute(text(f"SELECT * FROM `{filename}`")).fetchall()
+        rows = [dict(row._mapping) for row in result]
+        columns = list(rows[0].keys()) if rows else []
+    return jsonify({'columns': list(columns), 'rows': rows, 'filename': filename})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
